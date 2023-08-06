409fdbbd74587ec0d344036d7562241e365b48a6
zonda yang <u226699@gmail.com>
2023-08-07 01:23:45 +0800

    Add first answer of homework1 of test_beta

--- homework1/test_beta/main.c
+++ homework1/test_beta/main.c
@@ -5,7 +5,8 @@ static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
     if ((alignment & mask) == 0) { /* power of two? */
-        return MMMM;
+                                   // TODO: MMMM
+        return (sz & (~mask)) + ((sz & mask) != 0) * (mask + 1);
     }
     return (((sz + mask) / alignment) * alignment);
 }
@@ -16,5 +17,7 @@ int main()
     printf("ans: %d\n", align_up(121, 4));  // expect: 124
     printf("ans: %d\n", align_up(122, 4));  // expect: 124
     printf("ans: %d\n", align_up(123, 4));  // expect: 124
+    printf("ans: %d\n", align_up(124, 4));  // expect: 124
+    printf("ans: %d\n", align_up(125, 4));  // expect: 128
     return 0;
 }
